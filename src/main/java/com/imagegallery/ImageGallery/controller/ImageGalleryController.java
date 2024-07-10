c3eab961fc4181c99da11d69d78894ca927c1c0b
Francisco José Retamero Jaime <fjrj3d@gmail.com>
2024-07-10 20:26:24 +0200

    update dev branch

--- src/main/java/com/imagegallery/ImageGallery/controller/ImageGalleryController.java
+++ src/main/java/com/imagegallery/ImageGallery/controller/ImageGalleryController.java
@@ -1,10 +1,14 @@
 package com.imagegallery.ImageGallery.controller;
 
+import com.imagegallery.ImageGallery.model.Image;
 import com.imagegallery.ImageGallery.service.ImageGalleryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
 
 @RestController
 @RequestMapping("/api/v1")
@@ -12,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class ImageGalleryController {
     @Autowired
     ImageGalleryService imageGalleryService;
+
 
 
 }
--- src/main/java/com/imagegallery/ImageGallery/service/ImageGalleryService.java
+++ src/main/java/com/imagegallery/ImageGallery/service/ImageGalleryService.java
@@ -1,8 +1,12 @@
 package com.imagegallery.ImageGallery.service;
 
+import com.imagegallery.ImageGallery.model.Image;
 import com.imagegallery.ImageGallery.repository.IImageGalleryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.ArrayList;
 
 @Service
 public class ImageGalleryService {
