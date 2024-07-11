b7e08dafbd7eeb235f6fe3f6d0b6612a7b28f9f0
Francisco José Retamero Jaime <fjrj3d@gmail.com>
2024-07-11 13:53:14 +0200

    added method addImage

--- src/main/java/com/imagegallery/ImageGallery/controller/ImageGalleryController.java
+++ src/main/java/com/imagegallery/ImageGallery/controller/ImageGalleryController.java
@@ -3,12 +3,8 @@ package com.imagegallery.ImageGallery.controller;
 import com.imagegallery.ImageGallery.model.Image;
 import com.imagegallery.ImageGallery.service.ImageGalleryService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 
 @RestController
 @RequestMapping("/api/v1")
@@ -17,6 +13,8 @@ public class ImageGalleryController {
     @Autowired
     ImageGalleryService imageGalleryService;
 
-
-
+    @PostMapping (path = "/images")
+    public Image addNewImage(@RequestBody Image image){
+        return imageGalleryService.addNewImage(image);
+    }
 }
--- src/main/java/com/imagegallery/ImageGallery/service/ImageGalleryService.java
+++ src/main/java/com/imagegallery/ImageGallery/service/ImageGalleryService.java
@@ -4,14 +4,14 @@ import com.imagegallery.ImageGallery.model.Image;
 import com.imagegallery.ImageGallery.repository.IImageGalleryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.GetMapping;
 
-import java.util.ArrayList;
 
 @Service
 public class ImageGalleryService {
     @Autowired
     IImageGalleryRepository iImageGalleryRepository;
 
-
+    public Image addNewImage(Image image){
+        return iImageGalleryRepository.save(image);
+    }
 }
